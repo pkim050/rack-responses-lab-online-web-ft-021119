22945a8a16bb631a77161763405c7b2004a87e56
Patrick Kim <pkim050@gmail.com>
2019-03-27 15:13:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,10 @@
 require 'pry'
 class Application
   resp = Rack::Response.new
-  binding.pry
+  time = Time.now
+  if time.hour <= 12
+    resp.write "Good Morning!"
+  else
+    resp.write "Good Afternoon!"
+  end
 end
